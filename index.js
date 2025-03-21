104a5b3d6110967d2ffa96ec6420dcfe2e6b8d4a
jef08 <jasfielding@gmail.com>
2025-03-21 14:57:23 +0100

    users can now input a city and country. Next step is to switch between farenheit and celsius and tweak the style.

--- index.js
+++ index.js
@@ -1,44 +1,72 @@
-navigator.geolocation.getCurrentPosition(function(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+//window.addEventListener("load", () => {
+        //document.querySelector('.input-weather-div').style.display = 'none';
+        navigator.geolocation.getCurrentPosition(function(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
 
-    const weatherUrl = `https://wttr.in/${latitude},${longitude}?format=j1`;
+        const weatherUrl = `https://wttr.in/${latitude},${longitude}?format=j1`;
 
-    fetch(weatherUrl)
-    .then(response => response.json())
-    .then(data => {
-        const localLocation = data.nearest_area[0].areaName[0].value;
-        const localRegion = data.nearest_area[0].region[0].value;
-        const temp = data.current_condition[0].temp_F;
-        const description = data.current_condition[0].weatherDesc[0].value;
-        const humidity = data.current_condition[0].humidity;
-        const uvIndex = data.current_condition[0].uvIndex;
-        const windSpeed = data.current_condition[0].windspeedMiles;
-        const windDirection = data.current_condition[0].winddir16Point;
-        document.querySelector('.local-location').textContent = `${localLocation}, ${localRegion}`;
-        document.querySelector('.local-temp').textContent = `${temp} f`;
-        document.querySelector('.local-desc').textContent = `${description}`;
-        document.querySelector('.local-humidity').textContent = `${humidity}%`;
-        document.querySelector('.local-uv').textContent = `${uvIndex}`;
-        document.querySelector(`.local-wind`).textContent = `${windSpeed}mph`;
-        document.querySelector('.local-wind-dir').textContent = `${windDirection}`;
+        fetch(weatherUrl)
+        .then(response => response.json())
+        .then(data => {
+            const localLocation = data.nearest_area[0].areaName[0].value;
+            const localRegion = data.nearest_area[0].region[0].value;
+            const temp = data.current_condition[0].temp_F;
+            const description = data.current_condition[0].weatherDesc[0].value;
+            const humidity = data.current_condition[0].humidity;
+            const uvIndex = data.current_condition[0].uvIndex;
+            const windSpeed = data.current_condition[0].windspeedMiles;
+            const windDirection = data.current_condition[0].winddir16Point;
+            document.querySelector('.local-location').textContent = `${localLocation}, ${localRegion}`;
+            document.querySelector('.local-temp').textContent = `${temp}\u00B0f`;
+            document.querySelector('.local-desc').textContent = `${description}`;
+            document.querySelector('.humidity-desc').textContent = `Humidity:`
+            document.querySelector('.local-humidity').textContent = `${humidity}%`;
+            document.querySelector('.uv-desc').textContent = `UV Index:`;
+            document.querySelector('.local-uv').textContent = `${uvIndex}`;
+            document.querySelector('.wind-desc').textContent = `Wind Speed:`
+            document.querySelector(`.local-wind`).textContent = `${windSpeed}mph`;
+            document.querySelector('.wind-dir-desc').textContent = `Wind Direction:`
+            document.querySelector('.local-wind-dir').textContent = `${windDirection}`;
 
+        })
+        .catch(error => console.log('error', error));
     })
-    .catch(error => console.log('error', error));
+//})
+
+
+
+document.getElementById('submit-bttn').addEventListener('click', (event) => {
+    event.preventDefault(); //This keeps the form from submitting and reloading the page which would cause the local weather to be shown again//
+    const inputText = document.getElementById('text-box').value.trim();
+
+    if (inputText) {
+        const weatherUrl = `https://wttr.in/${inputText}?format=j1`;
+        fetch(weatherUrl)
+        .then(response => response.json())
+        .then(data => {
+            const localLocation = data.nearest_area[0].areaName[0].value;
+            const localRegion = data.nearest_area[0].region[0].value;
+            const temp = data.current_condition[0].temp_F;
+            const description = data.current_condition[0].weatherDesc[0].value;
+            const humidity = data.current_condition[0].humidity;
+            const uvIndex = data.current_condition[0].uvIndex;
+            const windSpeed = data.current_condition[0].windspeedMiles;
+            const windDirection = data.current_condition[0].winddir16Point;
+            document.querySelector('.local-location').textContent = `${localLocation}, ${localRegion}`;
+            document.querySelector('.local-temp').textContent = `${temp}\u00B0f`;
+            document.querySelector('.local-desc').textContent = `${description}`;
+            document.querySelector('.humidity-desc').textContent = `Humidity:`
+            document.querySelector('.local-humidity').textContent = `${humidity}%`;
+            document.querySelector('.uv-desc').textContent = `UV Index:`;
+            document.querySelector('.local-uv').textContent = `${uvIndex}`;
+            document.querySelector('.wind-desc').textContent = `Wind Speed:`
+            document.querySelector(`.local-wind`).textContent = `${windSpeed}mph`;
+            document.querySelector('.wind-dir-desc').textContent = `Wind Direction:`
+            document.querySelector('.local-wind-dir').textContent = `${windDirection}`;
+        })
+        .catch(error => console.log('error', error));
+    } else {
+        alert('Please enter a city and country')
+    }
 })
-
-
-
-fetch('https://wttr.in/ogden?format=j1')
-    .then(response => response.json())
-    .then(data => {
-        const temp = data.current_condition[0].temp_F;
-        const description = data.current_condition[0].weatherDesc[0].value;
-        const humidity = data.current_condition[0].humidity;
-        const uvIndex = data.current_condition[0].uvIndex;
-        const windSpeed = data.current_condition[0].windspeedMiles;
-        const windDirection = data.current_condition[0].winddir16Point;
-        document.querySelector('.input-weather').textContent = `Current temperature: ${temp}, ${description}, Humidity: ${humidity},
-        UV: ${uvIndex}, wind Speed: ${windSpeed}, wind direction: ${windDirection}`;
-    })
-    .catch(error => console.log('error', error));
